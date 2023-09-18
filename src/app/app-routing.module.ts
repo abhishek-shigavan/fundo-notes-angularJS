7c05d7133933166f7f72b82aa18752c1e3836299
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-09-18 14:19:48 +0530

    MODIFIED: added children route in dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NotesContainerComponent } from './components/notes-container/notes-container.component';
+import { TrashContainerComponent } from './components/trash-container/trash-container.component';
+import { ArchiveContainerComponent } from './components/archive-container/archive-container.component';
 
 const routes: Routes = [
   {path:"login", component: LoginComponent},
   {path:"signup", component: SignupComponent},
-  {path: "dashboard", component: DashboardComponent}
+  {path: "dashboard", component: DashboardComponent, children: [
+    {path: "notes", component: NotesContainerComponent},
+    {path: "archive", component: ArchiveContainerComponent },
+    {path: "trash", component: TrashContainerComponent},
+  ]}
 ];
 
 @NgModule({
