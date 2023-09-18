07e6fe5e63350fede1d760a16ed8e76b8c5e3324
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-09-18 14:25:38 +0530

    ADD: created header for dashboard

---
+++ src/app/components/dashboard-header/dashboard-header.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-header',
+  templateUrl: './dashboard-header.component.html',
+  styleUrls: ['./dashboard-header.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class DashboardHeaderComponent {
+
+}
