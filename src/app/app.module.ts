763eb8cd5632bf2ef3b3df5c2155d323f79c431b
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-09-20 16:57:44 +0530

    MODIFIED: updated imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 import { AppRoutingModule } from './app-routing.module';
 import { MatButtonModule } from '@angular/material/button';
@@ -53,7 +54,8 @@ import { NoteFooterIconsComponent } from './components/note-footer-icons/note-fo
     ReactiveFormsModule,
     MatSidenavModule,
     MatSelectModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
