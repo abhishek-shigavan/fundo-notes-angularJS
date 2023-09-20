382937ee80e71ee736fe50902432a539f116369c
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-09-20 16:45:59 +0530

    MODIFIED: subscribing to side navbar global state

--- src/app/components/side-nav-bar/side-nav-bar.component.ts
+++ src/app/components/side-nav-bar/side-nav-bar.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DataService } from 'src/app/services/data-service/data.service';
 
 @Component({
   selector: 'app-side-nav-bar',
   templateUrl: './side-nav-bar.component.html',
   styleUrls: ['./side-nav-bar.component.scss']
 })
-export class SideNavBarComponent {
+export class SideNavBarComponent implements OnInit, OnDestroy {
+  showSideNavbar!: boolean;
+  subscription!: Subscription;
+
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.subscription = this.data.currentSideNavbarState.subscribe(currState => this.showSideNavbar = currState)
+  }
+  
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
