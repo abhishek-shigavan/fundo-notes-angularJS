bd82042c37fa26176deb99ba3885565cfee5ba58
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-09-21 11:50:28 +0530

    MODIFIED: added host to pass class to selector

--- src/app/components/notes-container/notes-container.component.ts
+++ src/app/components/notes-container/notes-container.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-notes-container',
   templateUrl: './notes-container.component.html',
-  styleUrls: ['./notes-container.component.scss']
+  styleUrls: ['./notes-container.component.scss'],
+  host: {
+    class: 'app-notes-cnt',
+  },
 })
 export class NotesContainerComponent {
 
