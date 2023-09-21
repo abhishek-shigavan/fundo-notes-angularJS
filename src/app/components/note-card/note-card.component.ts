c398286298069287d475ac066179eb0f90a664c1
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-09-21 15:40:04 +0530

    MODIFIED: added class to selector

--- src/app/components/note-card/note-card.component.ts
+++ src/app/components/note-card/note-card.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-note-card',
   templateUrl: './note-card.component.html',
-  styleUrls: ['./note-card.component.scss']
+  styleUrls: ['./note-card.component.scss'],
+  host: {
+    class: 'note-card-display-cnt'
+  }
 })
 export class NoteCardComponent {
 
