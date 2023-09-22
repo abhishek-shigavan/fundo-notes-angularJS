450d66e04d8c996515a75b09cf3f45ce824f3050
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-09-22 15:14:57 +0530

    MODIFIED: accessing data coming from notes container

--- src/app/components/note-card/note-card.component.ts
+++ src/app/components/note-card/note-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
@@ -9,5 +9,7 @@ import { Component } from '@angular/core';
   }
 })
 export class NoteCardComponent {
+  @Input() notesList!: any;
+  constructor() { }
 
 }
