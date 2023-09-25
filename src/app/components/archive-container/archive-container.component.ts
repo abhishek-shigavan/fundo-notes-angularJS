39d121361ab492159afa13058a3a929312944eea
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-09-25 23:55:48 +0530

    MODIFIED: fetching archive notes

--- src/app/components/archive-container/archive-container.component.ts
+++ src/app/components/archive-container/archive-container.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NoteService } from 'src/app/services/note-service/note.service';
 
 @Component({
   selector: 'app-archive-container',
   templateUrl: './archive-container.component.html',
-  styleUrls: ['./archive-container.component.scss']
+  styleUrls: ['./archive-container.component.scss'],
+  host: {
+    class: 'app-archive-notes-cnt'
+  }
 })
-export class ArchiveContainerComponent {
+export class ArchiveContainerComponent implements OnInit {
+  archiveNotesArray: any = []
 
+  constructor(private noteService: NoteService) {}
+  
+  ngOnInit() {
+    this.noteService.getAllArchiveNotes().subscribe(
+      (respose) => {
+        this.archiveNotesArray = [...respose.data?.data]
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  }
 }
