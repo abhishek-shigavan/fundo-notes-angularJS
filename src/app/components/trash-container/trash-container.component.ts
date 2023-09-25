d7228c833a71630361bd2c88b369d5899427c770
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-09-25 23:48:16 +0530

    MODIFIED: getting trash notes & storing it

--- src/app/components/trash-container/trash-container.component.ts
+++ src/app/components/trash-container/trash-container.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NoteService } from 'src/app/services/note-service/note.service';
 
 @Component({
   selector: 'app-trash-container',
   templateUrl: './trash-container.component.html',
-  styleUrls: ['./trash-container.component.scss']
+  styleUrls: ['./trash-container.component.scss'],
+  host: {
+    class: 'app-trash-cnt'
+  }
 })
-export class TrashContainerComponent {
-
+export class TrashContainerComponent implements OnInit {
+  trashNotesArray: any = []
+  
+  constructor(public noteService: NoteService) {}
+  ngOnInit() {
+    this.noteService.getAllTrashNotes().subscribe(
+      (response) => {
+        this.trashNotesArray = [...response.data?.data]
+      },
+      (err) => {
+        console.log(err)
+      }
+    )
+  }
 }
