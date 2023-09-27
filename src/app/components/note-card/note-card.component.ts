687161b0c7e5aca25ef9afef565691ef2aef40d9
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-09-27 23:47:19 +0530

    MODIFIED: emiting the event to update respective notes list

--- src/app/components/note-card/note-card.component.ts
+++ src/app/components/note-card/note-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
@@ -6,10 +6,16 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./note-card.component.scss'],
   host: {
     class: 'note-card-display-cnt'
-  }
+  },
+  // encapsulation: ViewEncapsulation.None,
 })
 export class NoteCardComponent {
+  applyStyle = true
   @Input() notesList!: any;
+  @Output() handleNotesOperations = new EventEmitter<Object>()
   constructor() { }
 
+  handleNotesOperation($event: any) {
+    this.handleNotesOperations.emit($event)
+  }
 }
