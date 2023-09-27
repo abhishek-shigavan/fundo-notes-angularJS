e58fb23de7e463ea5dbf8a7b321e74d2e632e598
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-09-27 15:07:47 +0530

    MODIFIED: accessing the current route to render the footer icons

--- src/app/components/note-footer-icons/note-footer-icons.component.ts
+++ src/app/components/note-footer-icons/note-footer-icons.component.ts
@@ -1,10 +1,11 @@
+import { state } from '@angular/animations';
 import { Component, Input, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Subscription } from 'rxjs';
 import { DataService } from 'src/app/services/data-service/data.service';
 import { NoteService } from 'src/app/services/note-service/note.service';
-import { REMINDER_ICON, COLLABRATOR_ICON, COLOR_PALATTE_ICON, IMG_ICON, ARCHIVE_ICON, MORE_ICON } from 'src/assets/svg-icons';
+import { REMINDER_ICON, COLLABRATOR_ICON, COLOR_PALATTE_ICON, IMG_ICON, ARCHIVE_ICON, MORE_ICON, DELETE_FOREVER_ICON, RESTORE_ICON, UNARCHIVE_ICON } from 'src/assets/svg-icons';
 
 @Component({
   selector: 'app-note-footer-icons',
@@ -12,10 +13,12 @@ import { REMINDER_ICON, COLLABRATOR_ICON, COLOR_PALATTE_ICON, IMG_ICON, ARCHIVE_
   styleUrls: ['./note-footer-icons.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class NoteFooterIconsComponent {
+
+export class NoteFooterIconsComponent implements OnInit, OnDestroy {
   @Input() enableStyle!: boolean;
   @Input() noteDetails: any
-  // subscription!: Subscription;
+  iconsContainer: string = ""
+  subscription!: Subscription;
 
   constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, private dataService: DataService, private noteService: NoteService) {
     iconRegistry.addSvgIconLiteral('reminder-icon', sanitizer.bypassSecurityTrustHtml(REMINDER_ICON));
@@ -24,14 +27,18 @@ export class NoteFooterIconsComponent {
     iconRegistry.addSvgIconLiteral('img-icon', sanitizer.bypassSecurityTrustHtml(IMG_ICON));
     iconRegistry.addSvgIconLiteral('archive-icon', sanitizer.bypassSecurityTrustHtml(ARCHIVE_ICON));
     iconRegistry.addSvgIconLiteral('more-icon', sanitizer.bypassSecurityTrustHtml(MORE_ICON));
+    iconRegistry.addSvgIconLiteral('delete-forever-icon', sanitizer.bypassSecurityTrustHtml(DELETE_FOREVER_ICON));
+    iconRegistry.addSvgIconLiteral('restore-icon', sanitizer.bypassSecurityTrustHtml(RESTORE_ICON));
+    iconRegistry.addSvgIconLiteral('unarchive-icon', sanitizer.bypassSecurityTrustHtml(UNARCHIVE_ICON));
   }
 
-  // ngOnInit() {
-  // }
-  
-  // ngOnDestroy() {
-  //   throw new Error('Method not implemented.');
-  // }
+  ngOnInit() {
+    this.subscription = this.dataService.currentSelectedRoute.subscribe(state => this.iconsContainer = state)
+  }
+   
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
+  }
 
   async handleArchiveOperation() {
     const res = await this.noteService.archiveNote({"noteIdList": [this.noteDetails?.id], "isArchived": true})
