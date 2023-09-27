1733819a0074978f2fd7cfcd2c18f0c667b25425
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-09-27 15:03:46 +0530

    MODIFIED: updating the current route state & filtering out only archive notes

--- src/app/components/archive-container/archive-container.component.ts
+++ src/app/components/archive-container/archive-container.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { DataService } from 'src/app/services/data-service/data.service';
 import { NoteService } from 'src/app/services/note-service/note.service';
 
 @Component({
@@ -11,17 +14,20 @@ import { NoteService } from 'src/app/services/note-service/note.service';
 })
 export class ArchiveContainerComponent implements OnInit {
   archiveNotesArray: any = []
+  subscription!: Subscription
 
-  constructor(private noteService: NoteService) {}
+  constructor(private noteService: NoteService, public route: ActivatedRoute, private dataService: DataService) {}
   
   ngOnInit() {
     this.noteService.getAllArchiveNotes().subscribe(
       (respose) => {
-        this.archiveNotesArray = [...respose.data?.data]
+        const filterArchiveNotes = respose?.data?.data.filter((item : { isDeleted : boolean }) => item.isDeleted == false)
+        this.archiveNotesArray = [...filterArchiveNotes]
       },
       (error) => {
         console.log(error)
       }
     )
+    this.dataService.updateCurrentRoute(this.route.snapshot?.routeConfig?.path || "")
   }
 }
