b0218a0eba8d5c8c332c79441ab0b4fbfa259c55
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-10-03 16:33:05 +0530

    ADD: created modal component to update note, iinjecting data while loading modal, calling update note api

---
+++ src/app/components/edit-note-modal/edit-note-modal.component.ts
@@ -0,0 +1,41 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { NoteService } from 'src/app/services/note-service/note.service';
+
+@Component({
+  selector: 'app-edit-note-modal',
+  templateUrl: './edit-note-modal.component.html',
+  styleUrls: ['./edit-note-modal.component.scss']
+})
+export class EditNoteModalComponent implements OnInit {
+  oldTitle: string = ""
+  oldDescription: string = ""
+
+  constructor(public dialogRef: MatDialogRef<EditNoteModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { title: string, description: string, id: string },
+    public noteService: NoteService) { 
+      this.oldTitle = data.title
+      this.oldDescription = data.description
+    }
+
+  ngOnInit(): void {
+    this.adjustTextareaHeight()
+  }
+
+  adjustTextareaHeight() {
+    const textarea = document.querySelector('textarea');
+    
+    if (textarea) {
+      textarea.style.height = 'auto';
+      textarea.rows = 4
+      textarea.style.height = `${textarea.scrollHeight}px`;
+    }
+  }
+
+  async handleCloseEditNote() {
+    if (this.data.title !== this.oldTitle || this.data.description !== this.oldDescription) {
+      const res = await this.noteService.editNote({...this.data, noteId: this.data?.id})
+    }
+    this.dialogRef.close();
+  }
+}
