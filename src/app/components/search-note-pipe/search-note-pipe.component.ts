4ef519f5af532213e294047bdb78105f04a87071
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-10-04 16:57:01 +0530

    MODIFIED: created pipe to filter notes according to search text

--- src/app/components/search-note-pipe/search-note-pipe.component.ts
+++ src/app/components/search-note-pipe/search-note-pipe.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
-@Component({
-  selector: 'app-search-note-pipe',
-  templateUrl: './search-note-pipe.component.html',
-  styleUrls: ['./search-note-pipe.component.scss']
+@Pipe({
+  name: 'searchPipe',
 })
-export class SearchNotePipeComponent {
-
+export class SearchNotePipeComponent implements PipeTransform {
+  transform(items: any, searchString: string) {
+    if(!items) return []
+    if(!searchString) return items
+    searchString = searchString.toLowerCase()
+    return items.filter((item: {title: string, description: string}) => item.title.toLowerCase().includes(searchString) || item.description.toLowerCase().includes(searchString));
+  }
 }
