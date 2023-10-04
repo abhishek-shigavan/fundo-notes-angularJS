bf2038de883231a7215ec06ad64320462d26efc9
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-10-04 10:47:17 +0530

    MODIFIED: checking the notes container & opening the edit note dialog

--- src/app/components/note-card/note-card.component.ts
+++ src/app/components/note-card/note-card.component.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import { Subscription } from 'rxjs';
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import { EditNoteModalComponent } from '../edit-note-modal/edit-note-modal.component';
+import { DataService } from 'src/app/services/data-service/data.service';
 
 @Component({
   selector: 'app-note-card',
@@ -11,13 +13,20 @@ import { EditNoteModalComponent } from '../edit-note-modal/edit-note-modal.compo
   },
   // encapsulation: ViewEncapsulation.None,
 })
-export class NoteCardComponent {
+export class NoteCardComponent implements OnInit {
   applyStyle = true
+  notesContainer: string = '';
+  subscribe!: Subscription;
   @Input() notesList!: any;
   @Output() handleNotesOperations = new EventEmitter<Object>()
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog, public dataService: DataService) {}
+  
+  ngOnInit() {
+    this.subscribe = this.dataService.currentSelectedRoute.subscribe(state => this.notesContainer = state)
+  }
 
   openDialog(noteDetails: Object) {
+    if (this.notesContainer == "trash") return
     const dialogRef = this.dialog.open(EditNoteModalComponent, {data: {...noteDetails}});
     dialogRef.afterClosed().subscribe(result => {
       this.handleNotesOperation({operation: "update", noteDetails: {...result}})
