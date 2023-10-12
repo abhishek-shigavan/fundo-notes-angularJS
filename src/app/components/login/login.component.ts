cfb5535fccf3f30aec73ea1f03a88d4a02720e43
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-10-12 11:44:03 +0530

    MODIFIED: added snackbar for invalid username / password

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user-service/user.service';
 
@@ -9,23 +10,16 @@ import { UserService } from 'src/app/services/user-service/user.service';
   styleUrls: ['./login.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   loginForm: FormGroup;
   submitted = false;
 
-  constructor(private formBuilder: FormBuilder, private loginService: UserService, public router: Router) {
+  constructor(public formBuilder: FormBuilder, private loginService: UserService, public router: Router, private snackBar: MatSnackBar) {
     this.loginForm = this.formBuilder.group({
       userName: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
-  }
-
-  ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      userName: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-    });
-  }
+  } 
 
   get userName() {
     return this.loginForm.get('userName');
@@ -33,6 +27,14 @@ export class LoginComponent implements OnInit {
 
   get password() {
     return this.loginForm.get('password');
+  }
+
+  openFailedLoginMsg() {
+    this.snackBar.open('Invalid Username / Password', "",{
+      duration: 5000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom'
+    });
   }
 
   async handleLogin() {
@@ -43,6 +45,11 @@ export class LoginComponent implements OnInit {
 
     const { userName, password } = this.loginForm.value
     const res = await this.loginService.login({ "email": userName, "password": password })
+    if(res?.status) {
+      this.openFailedLoginMsg()
+      this.loginForm.reset()
+    }
+
     if(res?.id) this.router.navigate(["dashboard/notes"]) 
   }
 }
