a0a85ac64294e8ec6255e2cc26beebe201ec136b
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-10-16 15:21:52 +0530

    MODIFIED: handling footer icons clicks for edit note

--- src/app/components/edit-note-modal/edit-note-modal.component.ts
+++ src/app/components/edit-note-modal/edit-note-modal.component.ts
@@ -10,12 +10,17 @@ import { NoteService } from 'src/app/services/note-service/note.service';
 export class EditNoteModalComponent implements OnInit {
   oldTitle: string = ""
   oldDescription: string = ""
+  oldColor: string = ""
+  oldArchive: boolean = false
+  mode: string = "EditNote"
 
   constructor(public dialogRef: MatDialogRef<EditNoteModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: { title: string, description: string, id: string },
+    @Inject(MAT_DIALOG_DATA) public data: { title: string, description: string, id: string, color: string, isArchive: boolean },
     public noteService: NoteService) { 
       this.oldTitle = data.title
       this.oldDescription = data.description
+      this.oldColor = data.color
+      this.oldArchive = data.isArchive
     }
 
   ngOnInit(): void {
@@ -32,10 +37,30 @@ export class EditNoteModalComponent implements OnInit {
     }
   }
 
+  async handleEditNoteFooterOperations(operation: any) {
+    let res
+    if (operation == "archive" || operation == 'unarchive') {
+      res = await this.noteService.archiveNote({
+        noteIdList: [this.data.id],
+        isArchived: operation == "archive" ? true : false,
+      });
+      this.data.isArchive = operation == "archive" ? true : false
+    } else if (operation.includes('#')) {
+      res = await this.noteService.colorChange({
+        noteIdList: [this.data?.id],
+        color: operation
+      })
+      this.data.color = operation
+    }
+  }
+
   async handleCloseEditNote() {
+    let operation = "update"
     if (this.data.title !== this.oldTitle || this.data.description !== this.oldDescription) {
       const res = await this.noteService.editNote({...this.data, noteId: this.data?.id})
     }
-    this.dialogRef.close(this.data);
+    if(this.data.isArchive !== this.oldArchive) operation = this.data.isArchive ? "archive" : "unarchive"
+    
+    this.dialogRef.close({operation: operation, noteData: this.data});
   }
 }
